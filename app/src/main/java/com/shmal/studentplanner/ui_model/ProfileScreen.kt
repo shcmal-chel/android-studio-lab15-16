7c00b21c3206b64b36e9aa4357c19b65bebda25c
shmal-chel <shcmalivan@gmail.com>
2026-04-06 22:32:54 +0300

    Step 7: Setup NavHost and complete navigation

--- app/src/main/java/com/shmal/studentplanner/ui_model/ProfileScreen.kt
+++ app/src/main/java/com/shmal/studentplanner/ui_model/ProfileScreen.kt
@@ -63,12 +63,12 @@ fun ProfileScreen(
                     )
                     Spacer(modifier = Modifier.height(16.dp))
                     Text(
-                        text = "Иванов Иван Иванович",
+                        text = "Шмаль Иван Максимович",
                         style = MaterialTheme.typography.headlineSmall,
                         fontWeight = FontWeight.Bold
                     )
                     Text(
-                        text = "Группа: ИСП-234",
+                        text = "Группа: ИСП-232",
                         style = MaterialTheme.typography.titleMedium,
                         color = MaterialTheme.colorScheme.onPrimaryContainer
                     )
@@ -89,7 +89,7 @@ fun ProfileScreen(
                     ProfileInfoRow(
                         icon = Icons.Default.Email,
                         label = "Email",
-                        value = "ivanov@vgi.volsu.ru"
+                        value = "shcmalivan@gmail.com"
                     )
                     HorizontalDivider()
                     ProfileInfoRow(
